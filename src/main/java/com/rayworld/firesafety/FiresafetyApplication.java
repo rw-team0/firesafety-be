daf4e71298c75794046dd2c78da18eb06177b9c2
soyoung <ssssy93@icloud.com>
2026-07-20 09:03:36 +0900

    init: 프로젝트 초기 생성

---
+++ src/main/java/com/rayworld/firesafety/FiresafetyApplication.java
@@ -0,0 +1,13 @@
+package com.rayworld.firesafety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiresafetyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiresafetyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rayworld/firesafety/FiresafetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayworld.firesafety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiresafetyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
